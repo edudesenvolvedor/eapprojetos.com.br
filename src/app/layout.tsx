b77375f4b777c165de2e0172233dbff48ed981ea
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2025-04-01 17:52:05 -0300

    feat: add components in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { ReactNode } from 'react';
+import { Navbar } from '@/components/Navbar';
+import Link from 'next/link';
+import { MessageSquareText, Moon } from 'lucide-react';
+import { ButtonMotion } from '@/components/ButtonMotion';
+import { NavLink } from '@/components/Navlink';
+import { Aside } from '@/components/Aside';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,7 +20,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased bg-base-light`}>
+        <Aside
+          firstname={'Eduardo'}
+          lastname={'Pacheco'}
+          specialization={'Dev. Full-Stack'}
+          basedIn={'Pará, Brasil'}
+        />
+        <Navbar
+          brand={
+            <Link href="/" className="text-gradient font-bold text-3xl ">
+              EAP Projetos
+            </Link>
+          }
+          rightButton={
+            <>
+              <button className="hidden xl:block p-2 text-t-disabled-light group cursor-pointer">
+                <Moon className="group-hover:text-t-bright-light" />
+              </button>
+              <ButtonMotion>
+                <span>Vamos Conversa</span>
+                <MessageSquareText />
+              </ButtonMotion>
+            </>
+          }
+        >
+          <NavLink isActive>Início</NavLink>
+          <NavLink>Portfólio</NavLink>
+          <NavLink>Sobre Mim</NavLink>
+          <NavLink>Currículo</NavLink>
+          <NavLink>Contato</NavLink>
+        </Navbar>
+
+        <div className="max-w-10/12 ml-auto mt-32 px-8">{children}</div>
+      </body>
     </html>
   );
 }
