6160327c47c60dcdabfc8babbd6c5d83116ab599
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2025-04-03 08:27:33 -0300

    feat: add navlink component

---
+++ src/components/Navbar/navlink.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, FC } from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+interface IProps extends VariantProps<typeof style> {
+  children?: ReactNode;
+  className?: string;
+}
+
+const style = cva(
+  "py-3 px-6 rounded-full cursor-pointer relative before:content-[''] before:absolute before:rounded-full before:inset-0 before:w-full before:h-full before:backdrop-blur-sm before:z-[-1]",
+  {
+    variants: {
+      isActive: {
+        true: 'text-t-bright-light border border-stroke-elements-light',
+        false: 'border border-transparent text-t-disabled-light hover:text-t-bright-light',
+      },
+    },
+    defaultVariants: {
+      isActive: false,
+    },
+  },
+);
+
+export const Navlink: FC<IProps> = ({ children, isActive = false, className }: IProps) => {
+  return <li className={style({ isActive, className })}>{children}</li>;
+};
