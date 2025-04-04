8299b43f02366580a19e609bea373737928aeea2
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2025-04-04 08:11:59 -0300

    fix: fix responsiveness of component

--- src/components/Timeline/milestone.tsx
+++ src/components/Timeline/milestone.tsx
@@ -1,32 +1,35 @@
 import { cva, type VariantProps } from 'class-variance-authority';
 import { FC, HTMLAttributes } from 'react';
 
-const styles = cva('border-b py-4 flex flex-wrap items-center justify-between md:*:w-auto', {
-  variants: {
-    variant: {
-      default: 'border-stroke-elements-light',
-      primary: 'border-blue-500',
-      success: 'border-green-500',
-      warning: 'border-yellow-500',
-      error: 'border-red-500',
+const styles = cva(
+  'border-b py-4 flex flex-wrap lg:flex-nowrap items-center md:*:w-auto text-start gap-3',
+  {
+    variants: {
+      variant: {
+        default: 'border-stroke-elements-light',
+        primary: 'border-blue-500',
+        success: 'border-green-500',
+        warning: 'border-yellow-500',
+        error: 'border-red-500',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
     },
   },
-  defaultVariants: {
-    variant: 'default',
-  },
-});
+);
 
 interface IProps extends HTMLAttributes<HTMLDivElement>, VariantProps<typeof styles> {
   year: string;
   title: string;
-  institution: string;
+  label?: string;
   description: string;
 }
 
 export const Milestone: FC<IProps> = ({
   year,
   title,
-  institution,
+  label,
   description,
   variant,
   className,
@@ -34,12 +37,12 @@ export const Milestone: FC<IProps> = ({
 }: IProps) => {
   return (
     <div className={styles({ variant, className })} {...props}>
-      <div className="text-lg text-t-muted-light font-semibold w-full md:max-w-2/12">{year}</div>
-      <div className="flex flex-col w-full py-5">
+      <div className="text-lg text-t-muted-light font-semibold min-w-2/12">{year}</div>
+      <div className="flex flex-col py-5 min-w-5/12">
         <span className="font-bold">{title}</span>
         <span className="text-sm text-gray-500">{label}</span>
       </div>
-      <p className="text-sm mt-2 w-full md:max-w-4/12">{description}</p>
+      <p className="text-sm lg:text-[18px]  mt-2 min-w-5/12">{description}</p>
     </div>
   );
 };
